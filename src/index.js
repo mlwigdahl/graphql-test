1299cc7d66f348948738864ca39f7188ae287a5b
Matt Wigdahl <matt@scriptpro.com>
2018-04-19 11:09:54 -0500

    reworking to check out subscriptions

--- src/index.js
+++ src/index.js
@@ -1,53 +1,31 @@
-const { GraphQLServer } = require('graphql-yoga');
-
-// fake data...
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Full stack tutorial for GraphQL using multiple implementation stacks',
-}];
-
-let idCount = links.length;
+const { printSchema } = require('graphql');
+const { GraphQLServer, PubSub } = require('graphql-yoga');
 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hacker News clone`,
-        feed: () => links,
-        link: (root, args) => {
-            const a = links.filter(v => v.id === args.id);
-            if (a.length === 0) return null;
-            return a[0];
-        }
     },
-    Mutation: {
-        post: (root, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            }
-            links.push(link);
-            return link;
+    Counter: {
+        countStr: counter => `Current count: ${counter.count}`,
+    },
+    Subscription: {
+        counter: {
+            subscribe: (parent, args, { pubsub }) => {
+                const channel = Math.random().toString(36).substring(2, 15); // random channel name
+                let count = 0;
+                setInterval(() => pubsub.publish(channel, { counter: { count: count++} }), 2000)
+                return pubsub.asyncIterator(channel);
+            },
         },
-        updateLink: (root, args) => {
-            const a = links.filter(v => v.id === args.id);
-            if (a.length === 0) return null;
-            if (args.url !== null && args.url !== undefined) a[0].url = args.url;
-            if (args.description !== null && args.description != undefined) a[0].description = args.description;
-            return a[0];
-        },
-        deleteLink: (root, args) => {
-            const a = links.filter(v => v.id === args.id);
-            if (a.length === 0) return null;
-            links = links.filter(v => v.id !== args.id);
-            return a[0];
-        }
-    }
+    },
 };
+
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
     typeDefs: `./src/schema.graphql`,
     resolvers,
+    context: { pubsub }
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
